0b950dc707434c282ff5bbbfe2da7ef997477918
adarshsosale <adarshg09@gmail.com>
2016-06-03 20:11:44 +0530

    Add files via upload

--- avg.py
+++ avg.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
+import re
 
 with open('avg.txt' , 'r') as avg:
     soup3 = BeautifulSoup(avg, "html.parser")
-    for span in soup3:
-        soup3.span.unwrap()
-        print(soup3)
+    strings = str(soup3)
+    price_list = int, re.findall(r'\d+', strings)
+    results = int(price_list)
+    print(results)
 
-def parseIntegers(mixedList):
-    return [x for x in testList if (isinstance(x, int) or isinstance(x, long)) and not isinstance(x, bool)]
