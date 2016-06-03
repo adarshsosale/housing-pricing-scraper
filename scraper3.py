ad45eaddee96fc4829489d1d1ffea200433742dd
adarshsosale <adarshg09@gmail.com>
2016-06-03 16:28:43 +0530

    Delete scraper3.py

--- scraper3.py
+++
@@ -1,7 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-soup = BeautifulSoup("<p> This is a test tag</p>")
-new_string = soup.p.string.wrap(soup.new_tag("b"))
-string =  str(new_string)
-print (string)
